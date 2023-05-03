5c83519dfde313f9000447f91b6d40efa2494293
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-04 00:34:41 +0600

    express installed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
 const allRecipies = require("./Data.json");
 
 const port = process.env.PORT || 5000;
